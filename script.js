77ab246c51471b89667735411b82ad96d6f522b8
pamela <abreupamm@gmail.com>
2022-03-29 00:04:52 -0300

    questao 6

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const cores = document.getElementsByClassName('ball');
 const resposta = document.getElementById('answer');
+const reset = document.getElementById('reset-game');
+const color = document.getElementById('rgb-color')
 
 function corCerta(event){
 const divClick = event.target;
@@ -11,12 +13,23 @@ resposta.innerHTML = 'Acertou!'
 
 }
 
+function gerarCor(){
 for (let i = 0; i < cores.length; i += 1) {
   const cor = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
   const p = cores[i];
   p.style.backgroundColor = cor;
   p.addEventListener('click', corCerta);
 }
+}
+gerarCor();
+
+function gerarNovasCores(){
+  gerarCor();
+  resposta.innerText = 'Escolha uma cor'
+  const cor = 'rgb (' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
+color.innerText = cor
+}
+reset.addEventListener('click', gerarNovasCores)
 
 
 // referencia para gerar cores aleátria : https://pt.stackoverflow.com/questions/493278/como-gerar-cores-hexadecimais-aleat%C3%B3rias-com-javascript
