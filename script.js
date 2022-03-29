6b33a53f33c5e373d0452b3821f55a3434f37970
pamela <abreupamm@gmail.com>
2022-03-29 12:00:15 -0300

    questao 6 corrigida

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 const cores = document.getElementsByClassName('ball');
 const resposta = document.getElementById('answer');
 const reset = document.getElementById('reset-game');
-const color = document.getElementById('rgb-color')
+const color = document.getElementById('rgb-color');
+let rgb = color.innerText;
 
-function corCerta(event){
-const divClick = event.target;
-if(divClick.style.backgroundColor == color){
-resposta.innerHTML = 'Acertou!'
-} else {
-  resposta.innerText = 'Errou! Tente novamente'
+function resultado(event) {
+  const divClick = event.target;
+  if (divClick.style.backgroundColor == rgb) {
+    resposta.innerHTML = 'Acertou!';
+  } else {
+    resposta.innerText = 'Errou! Tente novamente';
+  }
 }
 
-}
-
-function gerarCor(){
+function gerarCor() {
   for (let i = 0; i < cores.length; i += 1) {
-    const cor = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
+    const cor ='rgb(' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
     const p = cores[i];
     p.style.backgroundColor = cor;
-    p.addEventListener('click', corCerta);
+    p.addEventListener('click', resultado);
   }
-  const divCerta = parseInt(Math.random() * 5)
-  
-  cores[divCerta].style.backgroundColor = color.innerHTML
-  
 }
 gerarCor();
 
-function gerarNovasCores(){
-  gerarCor();
-  resposta.innerText = 'Escolha uma cor'
-  const cor = 'rgb (' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
-  color.innerText = cor
+function corCerta() {
+  const i = parseInt(Math.random() * 6);
+  const div2 = cores[i];
+  cores[i].style.backgroundColor = rgb;
 }
-reset.addEventListener('click', gerarNovasCores)
+corCerta();
 
+function gerarNovasCores() {
+  gerarCor();
+  resposta.innerText = 'Escolha uma cor';
+  const cor ='rgb(' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ', ' + parseInt(Math.random() * 255) + ')';
+  color.innerText = cor;
+  rgb = cor;
+  corCerta();
+}
+reset.addEventListener('click', gerarNovasCores);
 
 // referencia para gerar cores aleátria : https://pt.stackoverflow.com/questions/493278/como-gerar-cores-hexadecimais-aleat%C3%B3rias-com-javascript
